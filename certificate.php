3729cc4e65cdf6c989e1126d9883bef6bb1a740d
Ahmad Bagwi Rifai <ahmad.kdesain@gmail.com>
2017-01-16 23:17:23 +0700

    Perbaikan file certificate.php
    Pengurangan fungsi perhitungan tinggi atau y karena dibuat manual 400px

--- certificate.php
+++ certificate.php
@@ -1,32 +1,27 @@
 <?php
-	$nama = $_POST['nama'];
+	$nama = $_POST['namacetak'];
 	if (empty($nama)) {
 		$gambar = "./1.jpg";
 	}
 		else {
-		$gambar = "./xyz123.jpg";
+		$gambar = "./sertifikat.jpg";
 	}
 	$image = imagecreatefromjpeg($gambar);
 	$white = imageColorAllocate($image, 255, 255, 255);
 	$black = imageColorAllocate($image, 0, 0, 0);
 	$font = "./QuinchoScript_PersonalUse.ttf";
 	$size = 42;
-	//definisikan panjang dan lebar gambar
+	//definisikan lebar gambar agar posisi teks selalu ditengah berapapun jumlah hurufnya
 	$image_width = imagesx($image);  
-	$image_height = imagesy($image);
 	//membuat textbox agar text centered
 	$text_box = imagettfbbox($size,0,$font,$nama);
 	$text_width = $text_box[2]-$text_box[0]; // lower right corner - lower left corner
 	$text_height = $text_box[3]-$text_box[1];
-
 	$x = ($image_width/2) - ($text_width/2);
-	$y = ($image_height/2) - ($text_height/2);
-
-	imagettftext($image, $size, 0, $x, $y, $black, $font, $nama);
+	//generate sertifikat beserta namanya
+	imagettftext($image, $size, 0, $x, 400, $black, $font, $nama);
 	//tampilkan di browser
 	header("Content-type:  image/jpeg");
 	imagejpeg($image);
 	imagedestroy($image);
 ?>
-
-
