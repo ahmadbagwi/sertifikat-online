af3da072c4a48ac1be0f27e3cc4cef4fcf548455
ahmad-x <ahmadbagwi.id@gmail.com>
2021-02-19 23:03:24 +0700

    update to mysqli,

--- index.php
+++ index.php
@@ -1,69 +1,56 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+require 'db.php';
+?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Certificate Online</title>
-	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-</head>
-<body>
-	<div class="container">
-		<div class="col-md-6 col-md-offset-3">
-			<div class="panel panel-primary">
-	  			<div class="panel-heading"><h2>Certificate World Culture Forum 2016</h2></div>
-	 				<div class="panel-body">
-						<div class="konten">
+	<head>
+		<title>Certificate Online</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="panel panel-primary">
+		  			<div class="panel-heading"><h2>Simple Certificate Online</h2></div>
+		 				<div class="panel-body">
+							<div class="konten">
+								<form action="index.php" method="post">
+									<strong>Please enter your name</strong><br>
+									<input type="text" class="form-control" id="keyword" name="keyword" placeholder="first name or last name" ></input>
+									<input type="submit" class="btn btn-primary" value="Check" name="certificate"></input><br><br><br>
+								</form>
+								<?php
+									if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+										$keyword = $_POST["keyword"];
 
-							<form action="index.php" method="post">
-								<strong>Please enter your name</strong><br>
-								<input type="text" class="form-control" name="id" placeholder="first name or last name" ></input>
-								<input type="submit" class="btn btn-primary" value="Check" name="certificate"></input><br><br><br>
-							</form>
-
-<?php
-	$getid = $_POST["id"];
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db   = "dbsertifikat";
-	//buat koneksi dan ambil database		
-	$koneksi = mysql_connect($host, $user, $pass) or die("Koneksi error");
-	$db = mysql_select_db($db) or die("database tidak ditemukan");
-
-	//ambil POST dan sesuaikan dengan database
-	$ambildb = mysql_query("SELECT nama FROM peserta WHERE no_registrasi LIKE '%$getid%' OR nama LIKE '%$getid%'");
-	
-	while ($row = mysql_fetch_assoc($ambildb)){
-		$hasil = $row['nama'];
-		
-	}
-
-?>
-							<form action="certificate.php" method="post">
-								<strong>It's your name?</strong><br>
-								<input type="text" class="form-control" name="namadisable" disabled="yes" value="<?php
-								if (!empty($getid)) { //berfungsi verifikasi, menampilkan nama hasil input saja tapi tidak dikirim ke untuk dicetak (certificate.php) karena status input tersebut disable
-								 	echo "$hasil";
-								 	}
-								 	else {
-								 	echo "your name will shown automatic";	
-								 	} 
-								   ?>" ></input>
-								<input type="hidden" name="namacetak" value="<?php
-								if (!empty($getid)) { //berfungsi mengirim nama yang telah diinput dan sesuai untuk di cetak ke (certificate.php)
-								 	echo "$hasil";
-								 	}
-								 	else {
-								 	echo "";	
-								 	} 
-								   ?>"></input>
-								<input id="get" type="submit" class="btn btn-primary" value="Get Certificate Now" name="certificate"></input>
-							</form>
-							<br><h4><strong>Notes:</strong> After certificate generated please save as file.jpg</h4>
+										$sql = "SELECT * FROM peserta WHERE nama LIKE '%".$keyword."%'";
+										$result = $conn->query($sql);
+										if ($result->num_rows > 0) {
+											while ($row = $result->fetch_assoc()){
+												$hasil = $row['nama'];
+											}
+										} else {
+											echo 'no result';
+										}
+									}
+									$conn->close();
+								?>
+								<form action="certificate.php" method="post">
+									<strong>It's your name?</strong><br>
+									<input type="text" class="form-control" name="namadisable" disabled="yes" value="<?php echo (isset($keyword))?$hasil:'your name will shown automatic';?>" >
+									<input type="hidden" name="namacetak" value="<?php echo (isset($keyword))?$hasil:'your name will shown automatic';?>">
+									<input id="get" type="submit" class="btn btn-primary" value="Get Certificate Now" name="certificate"></input>
+								</form>
+								<br><h4><strong>Notes:</strong> After certificate generated please save as file.jpg</h4>
+								<span>https://github.com/ahmadbagwi/sertifikat-online</span>
+							</div>
 						</div>
 					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-
-</body>
-
+	</body>
 </html>
